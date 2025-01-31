222eb68ac057a0ac98f0815f32bc92dbcc4e35c1
Farisah <ghasanifarisah01@gmail.com>
2025-01-31 14:17:45 +0700

    data alat ukur

---
+++ app/Models/Alatukur.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AlatUkur extends Model
+{
+    use HasFactory;
+    protected $table = 'list_alat_ukur';
+    protected $fillable = [
+        'urutan', 'RO', 'kode', 'nama_alat', 'merk', 'type', 'serial_number', 'tahun_perolehan',
+        'kondisi_alat', 'harga_pembelian', 'no_kontrak_spk'
+    ];
+}
+
+
